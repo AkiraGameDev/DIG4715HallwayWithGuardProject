0b61388ce2582a8ca76b398b3ba2192faee7d023
DominicLincourt <nicklincourt@gmail.com>
2020-02-24 17:28:22 -0500

    created main menu and renamed scenes

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    
+    public void Game()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
