846e2ffde59a2d26ce6176a3b190c39a015cf985
DominicLincourt <nicklincourt@gmail.com>
2020-02-26 17:54:58 -0500

    sign instructions

--- Assets/Scripts/Sign.cs
+++ Assets/Scripts/Sign.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class Sign : MonoBehaviour, IInteractable
 {
+    public GameObject instructions;
+    public GameObject camera;
     public string InteractabilityInfo { get { return "some text here"; } }
+    private MouseLook mouseLook;
+    private float xSens;
+    private float ySens;
+
+    void Start()
+    {
+        instructions.SetActive(false);
+        mouseLook = camera.GetComponent<MouseLook>();
+        xSens = mouseLook.xSensitivity;
+        ySens = mouseLook.ySensitivity;
+    }
 
     public void ShowInteractability()
     {
@@ -13,6 +26,19 @@ public class Sign : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        print ("hello");
+        instructions.SetActive(true);
+        mouseLook.xSensitivity = 0;
+        mouseLook.ySensitivity = 0;
+    }
+
+    void Update()
+    {
+        
+        if (Input.anyKeyDown && instructions.activeInHierarchy && !Input.GetKeyDown(KeyCode.E) && !Input.GetButtonDown("Fire1"))
+        {
+            instructions.SetActive(false);
+            mouseLook.xSensitivity = xSens;
+            mouseLook.ySensitivity = ySens;
+        }
     }
 }
